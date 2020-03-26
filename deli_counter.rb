a74ad54dcb30debd3d5e50576eb3a45a1df07446
Micah McCallum <micahmccallum@outlook.com>
2020-03-26 03:04:22 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -20,7 +20,7 @@ def take_a_number(array, name)
 end
 
 def now_serving(array)
-  if array.length
+  if !(array.length == 0)
     puts "Currently serving #{array.shift}."
   else
     puts "There is nobody waiting to be served!"
