971d48687c3cadb6eedddc5bf6af97ff49965428
Micah McCallum <micahmccallum@outlook.com>
2020-03-26 02:51:41 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -5,5 +5,10 @@ katz_deli = []
 def line(array)
   if array.length == 0
     puts "The line is currently empty."
-  end
+  else
+    line = "The line is currently:"
+    array.each_with_index do | customer, index |
+      line << " #{index + 1}. #{customer}"
+    end
+    puts line
 end
