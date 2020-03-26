708ba46b67ef1a689085301f3f0bb4e489fb28bc
Micah McCallum <micahmccallum@outlook.com>
2020-03-26 03:04:34 +0000

    Done.

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,1 +1,28 @@
 # Write your code here.
+katz_deli = []
+
+
+def line(array)
+  if array.length == 0
+    puts "The line is currently empty."
+  else
+    line = "The line is currently:"
+    array.each_with_index do | customer, index |
+      line << " #{index + 1}. #{customer}"
+    end
+    puts line
+  end
+end
+
+def take_a_number(array, name)
+  array.push(name)
+  puts "Welcome, #{name}. You are number #{array.length} in line."
+end
+
+def now_serving(array)
+  if !(array.length == 0)
+    puts "Currently serving #{array.shift}."
+  else
+    puts "There is nobody waiting to be served!"
+  end
+end
